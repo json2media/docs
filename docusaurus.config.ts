53f6c4de4169abc47ee8c0d0acd625c19414c01f
RahulVerma989 <RahulVerma989@users.noreply.github.com>
2024-12-17 06:48:47 +0530

    updated url and base url after connecting the custom domain name docs.json2media.com

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://json2media.github.io',
+  url: 'https://docs.json2media.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
